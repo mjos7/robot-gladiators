cbb944c2b92372a03a356fc0a0d5d76aaa253245
mjos7 <markjnjoseph@gmail.com>
2021-07-03 00:07:33 -0700

    Resolve Bugs and Add Final Enhancements

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,7 +15,7 @@ var fight = function (enemy) {
       'Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.'
     );
 
-    // if player picks "skip" `confirm and then stop the loop
+    // if player picks "skip" confirm and then stop the loop
     if (promptFight === 'skip' || promptFight === 'SKIP') {
       // confirm player wants to skip
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
@@ -32,7 +32,7 @@ var fight = function (enemy) {
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
+    // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
     enemy.health = Math.max(0, enemy.health - damage);
@@ -64,6 +64,7 @@ var fight = function (enemy) {
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
     playerInfo.health = Math.max(0, playerInfo.health - damage);
+
     console.log(
       enemy.name +
         ' attacked ' +
@@ -95,18 +96,18 @@ var startGame = function () {
 
   // fight each enemy robot by looping over them and fighting them one at a time
   for (var i = 0; i < enemyInfo.length; i++) {
-    // if player is still alive, keep fighting
+    // if player is still alive, keep fight next enemy
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
-      // pick new enemy to fight based on the index of the enemy.names array
+      // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
 
-      // reset enemy.health before starting new fight
+      // set health for picked enemy
       pickedEnemyObj.health = randomNumber(40, 60);
 
-      // pass the pickedenemy.name variable's value into the fight function, where it will assume the value of the enemy.name parameter
+      // pass the pickedEnemyObj object variable's value into the fight function, where it will assume the value of the enemy parameter
       fight(pickedEnemyObj);
 
       // if player is still alive and we're not at the last enemy in the array
@@ -129,7 +130,7 @@ var startGame = function () {
     }
   }
 
-  // after loop ends, we are either out of playerInfo.health or enemies to fight, so run the endGame function
+  // after loop ends, we are either out of player.health or enemies to fight, so run the endGame function
   endGame();
 };
 
@@ -158,6 +159,7 @@ var endGame = function () {
   }
 };
 
+// go to shop between battles function
 var shop = function () {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
@@ -183,7 +185,7 @@ var shop = function () {
     default:
       window.alert('You did not pick a valid option. Try again.');
 
-      // call shop() again to force player to pick a valid option
+      // calls shop() again to force player to pick a valid option
       shop();
       break;
   }
@@ -193,18 +195,30 @@ var shop = function () {
 
 /* GAME INFORMATION / VARIABLES */
 
-// player information
+// function to set name
+var getPlayerName = function () {
+  var name = '';
 
+  while (name === '' || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
+// player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
+
   reset: function () {
     this.health = 100;
     this.money = 10;
     this.attack = 10;
-  }, // comma!
+  },
   refillHealth: function () {
     if (this.money >= 7) {
       window.alert("Refilling player's health by 20 for 7 dollars.");
@@ -224,6 +238,7 @@ var playerInfo = {
     }
   },
 };
+
 // enemy information
 var enemyInfo = [
   {
@@ -239,6 +254,7 @@ var enemyInfo = [
     attack: randomNumber(10, 14),
   },
 ];
+
 /* END GAME INFORMATION / VARIABLES */
 
 /* RUN GAME */
